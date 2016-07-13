0dc8bab229ec4497fc7347c7246b7ad75c340f05
idonans <idonans@126.com>
2016-07-13 12:06:30 +0800

    add demo addTask

--- app/src/main/java/com/idonans/adownload/demo/MainActivity.java
+++ app/src/main/java/com/idonans/adownload/demo/MainActivity.java
@@ -1,13 +1,54 @@
 package com.idonans.adownload.demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.idonans.acommon.app.CommonActivity;
+import com.idonans.acommon.lang.CommonLog;
+import com.idonans.acommon.util.FileUtil;
+import com.idonans.acommon.util.ViewUtil;
+import com.idonans.adownload.ADownloadRequest;
+import com.idonans.adownload.ADownloadTask;
+import com.idonans.adownload.ADownloadTaskFetchCallback;
+
+import java.io.File;
+
+public class MainActivity extends CommonActivity {
+
+    private static final String TAG = "MainActivity";
+    private ADownloadRequest mDownloadRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View addTask = ViewUtil.findViewByID(this, R.id.add_task);
+        addTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addTask();
+            }
+        });
     }
+
+    private void addTask() {
+        final String url = "http://www.iteye.com/images/logo.gif?1448702469";
+        File downloadDir = FileUtil.getPublicDownloadDir();
+        if (downloadDir == null) {
+            Toast.makeText(this, "public download dir not found", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        mDownloadRequest = new ADownloadRequest(url, downloadDir.getAbsolutePath());
+        mDownloadRequest.enqueueToDownload(new ADownloadTaskFetchCallback() {
+            @Override
+            public void onDownloadTaskFetched(ADownloadTask.Snapshot snapshot) {
+                CommonLog.d(TAG + " onDownloadTaskFetched " + snapshot);
+                mDownloadRequest = null;
+            }
+        });
+    }
+
 }
